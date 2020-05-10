29493333540e8a69de4af08e36bd6ffad843f96b
Snehita-I-10 <53755280+Snehita-I-10@users.noreply.github.com>
2020-05-10 13:51:30 +0530

    Update treks.php

--- treks.php
+++ treks.php
@@ -32,20 +32,25 @@ require_once('operation.php');
 require_once('component.php');
 ?>
 <html>
+<link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <body>
-<div class=container">
-<form method="post" action="trekk.php" >
-    <h1 CLASS=" text-info text-center">INTREPID TREKKERS!</h1>
+<div class="container">
+    <form method="post" action="trekk.php" >
+        <h1 CLASS=" text-info text-center" style="padding-top:35px">INTREPID TREKKERS!</h1>
 
-    <h5 class="text-center text-white" style="padding-left: 50px;padding-right: 50px;">Discover our selection of more than 200 trekking tours and other adventure sports activities in different places all over the world!!. <br>Book your adventure trekking holidays at best prices and services.</h5>
-    <br>
-    <h4>Check out our upcoming treks...</h4>
-    <?php
-buttonElement("btn btn-info","Ladakh","ladakh");
-
-    ?>
-</form>
+        <h5 class="text-center text" style="padding-left: 50px;padding-right: 50px;font-family: 'Comic Neue', cursive;">Discover our selection of trekking tours and other adventure sports activities in different places all over India! <br>Book your adventure trekking holidays at best prices and services.</h5>
+        <br>
+        <br><br>
+        <h4 style="padding-left:30px;font-family: 'Comic Neue', bold;">Check out our upcoming treks...</h4>
+        <br>
+        <?php
+        buttonElement("btn-lg btn-info","Ladakh","ladakh");
+        buttonElement("btn-lg btn-info","Kashmir","kashmir");
+        buttonElement("btn-lg btn-info","Himachal Pradesh","hp");
+        buttonElement("btn-lg btn-info","Uttarkhand","uttarkhand");
+        ?>
+    </form>
 </div>
 <style>
     body{
@@ -53,6 +58,15 @@ buttonElement("btn btn-info","Ladakh","ladakh");
         background-size: cover;
         background-repeat: no-repeat;
     }
+    .btn-lg{
+        margin-left:30px;
+        margin-right:10px;
+        margin-bottom:20px;
+        height:2cm;
+        font-size:25;
+        font-family: 'Comic Neue', cursive;
+    }
+
 </style>
 </body>
 </html>

2475e90edd728c3073aa6a1b280f4499c8fa368e
Snehita-I-10 <53755280+Snehita-I-10@users.noreply.github.com>
2020-06-17 18:10:27 +0530

    Update equipment1.php

--- equipment1.php
+++ equipment1.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,82 +36,82 @@ $con = createdb();
 displayequip();
 function displayequip()
 {   $sql2 = "select s.user_id from signup s where s.loggedin='yes'";
-    $result2 = mysqli_query($GLOBALS['con'], $sql2);
-    if (mysqli_num_rows($result2) > 0) {
-        while ($row2 = mysqli_fetch_assoc($result2)) {
+$result2 = mysqli_query($GLOBALS['con'], $sql2);
+if (mysqli_num_rows($result2) > 0) {
+    while ($row2 = mysqli_fetch_assoc($result2)) {
 
-            $user_id = $row2['user_id'];
+        $user_id = $row2['user_id'];
 
 
-                               }
     }
-    $sql="select * from trekequipments";
-    $result = mysqli_query($GLOBALS['con'],$sql);
-        if(mysqli_num_rows($result)>0){?>
-        <div class="container">
-            <div class="row">
+}
+$sql="select * from trekequipments";
+$result = mysqli_query($GLOBALS['con'],$sql);
+if(mysqli_num_rows($result)>0){?>
+<div class="container">
+    <div class="row">
         <?php
-            while($row=mysqli_fetch_assoc($result))
-            {?>
-                <div style="padding-bottom: 20px;" class="col-lg-6">
+        while($row=mysqli_fetch_assoc($result))
+        {?>
+            <div style="padding-bottom: 20px;" class="col-lg-6">
                 <div class="card" style="width: 18rem;">
 
 
-  <div class="card-body" style="background-color: #2a2f27 ">
+                    <div class="card-body" style="background-color: #2a2f27 ">
 
 
 
 
 
-                <img style="height:300px" src="<?php echo $row['equip_image']?>" class="card-img-top" >
-                <h5 class="card-title text-white"><?php echo $row['equipment_name']  ?></h5>
-                <?php
-//               $name= $row['equipment_name'];
-//               echo $name;?><!--<br>-->
-                <?php
-               $eid= $row['equipment_id'];
-                ?>
-                <?php
-                $image=$row['equip_image'];
-                ?>
-                <?php
-              ?>
+                        <img style="height:300px" src="<?php echo $row['equip_image']?>" class="card-img-top" >
+                        <h5 class="card-title text-white"><?php echo $row['equipment_name']  ?></h5>
+                        <?php
+                        //               $name= $row['equipment_name'];
+                        //               echo $name;?><!--<br>-->
+                        <?php
+                        $eid= $row['equipment_id'];
+                        $name=$row['equipment_name'];
+                        ?>
+                        <?php
+                        $image=$row['equip_image'];
+                        ?>
+                        <?php
+                        ?>
 
-<!--                <img src="--><?php //echo $row['equip_image']?><!--" style="width:300px; height:300px">-->
-                <?php
-                $sql1="select p.amount,p.restrictions,t.equipment_id from trekequipments t,price p where t.equipment_id='$eid' and t.pricecode=p.price_code";
-                $result1 = mysqli_query($GLOBALS['con'],$sql1);
-                if(mysqli_num_rows($result1)>0) {
-                    while ($row1 = mysqli_fetch_assoc($result1)) {
-                        $price = $row1['amount'];?>
-                        <p class="card-text text-white"> PRICE:  <?php  echo $price;?></p>
-                        <p class="card-text text-white"> RESTRICTIONS: <?php  echo $row1['restrictions'];?></p>
-
-
-<!--                      --><?php // echo $price;
+                        <!--                <img src="--><?php //echo $row['equip_image']?><!--" style="width:300px; height:300px">-->
+                        <?php
+                        $sql1="select p.amount,p.restrictions,t.equipment_id from trekequipments t,price p where t.equipment_id='$eid' and t.pricecode=p.price_code";
+                        $result1 = mysqli_query($GLOBALS['con'],$sql1);
+                        if(mysqli_num_rows($result1)>0) {
+                            while ($row1 = mysqli_fetch_assoc($result1)) {
+                                $price = $row1['amount'];?>
+                                <p class="card-text text-white"> PRICE:  <?php  echo $price;?></p>
+                                <p class="card-text text-white"> RESTRICTIONS: <?php  echo $row1['restrictions'];?></p>
 
 
+                                <!--                      --><?php // echo $price;
 
-                    }}?>
 
 
-                      <form action="mycart.php?id=<?php echo $eid?>&name=<?php echo $name?>&price=<?php echo $price?>&image=<?php echo $image?>" method="post">
-                          <a name="add" href="mycart.php" class="btn btn-primary">ADD TO CART</a>
+                            }}?>
+
+                        href="mycart.php?id=<?php echo $eid?>&name=<?php echo $name?>&price=<?php echo $price?>&image=<?php echo $image?>"
+                        <form action="mycart.php?id=<?php echo $eid?>&name=<?php echo $name?>&price=<?php echo $price?>&image=<?php echo $image?>"" method="post">
+                            <button name="add"  class="btn btn-primary">ADD TO CART</button>
                         </form>
-  </div>
+                    </div>
                 </div>
-                </div>
-
-
-
-<?php
-
-            }?>
             </div>
-            </div>
+
+
+
+            <?php
+
+        }?>
+    </div>
+</div>
 </body>
 <?php
-        }
 }
-                   ?>
-
+}
+?>
